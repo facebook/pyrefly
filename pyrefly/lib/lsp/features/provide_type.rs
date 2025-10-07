c7fa597d3f0ba612e0113433fded02303852c94e
Danny Yang <yangdanny@meta.com>
2025-10-07 12:48:51 -0700

    set up request/response for provide-type LSP (#1258)
    Summary:
    
    for https://github.com/facebook/pyrefly/issues/1181
    
    Differential Revision: D84072685

---
+++ pyrefly/lib/lsp/features/provide_type.rs
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use lsp_types::MarkupContent;
+use lsp_types::Position;
+use lsp_types::TextDocumentIdentifier;
+use lsp_types::request::Request;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug)]
+pub enum ProvideType {}
+
+impl Request for ProvideType {
+    type Params = ProvideTypeParams;
+    type Result = Option<ProvideTypeResponse>;
+    const METHOD: &'static str = "types/provide-type";
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ProvideTypeParams {
+    pub text_document: TextDocumentIdentifier,
+    pub positions: Vec<Position>,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Deserialize, Serialize)]
+pub struct ProvideTypeResponse {
+    pub contents: Vec<MarkupContent>,
+}
