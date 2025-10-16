3e5aaafdd3dcd51d4a003e35b004aab03653836f
Asuka Minato <i@asukaminato.eu.org>
2025-10-17 03:36:41 +0900

    fix

--- pyrefly/lib/alt/class/class_metadata.rs
+++ pyrefly/lib/alt/class/class_metadata.rs
@@ -973,16 +973,21 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
                                 ErrorInfo::Kind(ErrorKind::InvalidArgument),
                                 "Second argument to NewType cannot be a protocol".to_owned(),
                             );
+                            None
+                        } else {
+                            Some((class_object, metadata))
                         }
-                    } else if metadata.is_new_type() {
-                        self.error(
-                            errors,
-                            range,
-                            ErrorInfo::Kind(ErrorKind::InvalidInheritance),
-                            "Subclassing a NewType not allowed".to_owned(),
-                        );
+                    } else {
+                        if metadata.is_new_type() {
+                            self.error(
+                                errors,
+                                range,
+                                ErrorInfo::Kind(ErrorKind::InvalidInheritance),
+                                "Subclassing a NewType not allowed".to_owned(),
+                            );
+                        }
+                        Some((class_object, metadata))
                     }
-                    Some((class_object, metadata))
                 }
             })
             .collect::<Vec<_>>()
