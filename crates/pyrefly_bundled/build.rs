dc4dc6ab1f0abe81ee2760023c0b626ec18f7801
Asuka Minato <i@asukaminato.eu.org>
2025-11-01 18:25:24 +0900

    done

--- crates/pyrefly_bundled/build.rs
+++ crates/pyrefly_bundled/build.rs
@@ -9,6 +9,7 @@ use std::env;
 use std::fs::File;
 use std::path::Path;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn get_input_path() -> PathBuf {
     match env::var_os("TYPESHED_ROOT") {
@@ -39,6 +40,15 @@ fn main() -> Result<(), std::io::Error> {
     // Only watch for metadata changes to avoid having Cargo repeatedly crawling for
     // changes in the entire typeshed dir.
     println!("cargo::rerun-if-changed=third_party/typeshed_metadata.json");
+
+    println!("cargo::rerun-if-changed=crates/pyrefly_bundled/generate_docstubs.py");
+
+    let status = Command::new("uv")
+        .args(["run", "generate_docstubs.py"])
+        .status()
+        .expect("failed to invoke uv");
+
+    assert!(status.success(), "`uv run …` returned non-zero");
 
     let input_path = get_input_path();
     let output_path = get_output_path().unwrap();
