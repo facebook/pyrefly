98a417c1daeff3d2d88e74cd8ab6096cc98c286c
Asuka Minato <i@asukaminato.eu.org>
2025-11-14 04:20:01 +0900

    fix

--- pyrefly/lib/lsp/non_wasm/server.rs
+++ pyrefly/lib/lsp/non_wasm/server.rs
@@ -205,6 +205,7 @@ use crate::lsp::non_wasm::module_helpers::module_info_to_uri;
 use crate::lsp::non_wasm::queue::HeavyTaskQueue;
 use crate::lsp::non_wasm::queue::LspEvent;
 use crate::lsp::non_wasm::queue::LspQueue;
+use crate::lsp::non_wasm::stdlib::is_python_stdlib_file;
 use crate::lsp::non_wasm::stdlib::should_show_stdlib_error;
 use crate::lsp::non_wasm::transaction_manager::TransactionManager;
 use crate::lsp::non_wasm::unsaved_file_tracker::UnsavedFileTracker;
