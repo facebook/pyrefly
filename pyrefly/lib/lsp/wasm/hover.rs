2991fe29195f00366a98c26fddd0459d682df7c9
Asuka Minato <i@asukaminato.eu.org>
2025-11-20 10:10:59 +0900

    fix

--- pyrefly/lib/lsp/wasm/hover.rs
+++ pyrefly/lib/lsp/wasm/hover.rs
@@ -25,6 +25,7 @@ use pyrefly_types::callable::Param;
 use pyrefly_types::callable::ParamList;
 use pyrefly_types::callable::Params;
 use pyrefly_types::callable::Required;
+use pyrefly_types::display::format_hover_code_snippet;
 use pyrefly_types::types::BoundMethodType;
 use pyrefly_types::types::Forallable;
 use pyrefly_types::types::Type;
@@ -233,27 +234,24 @@ impl HoverValue {
             .display
             .clone()
             .unwrap_or_else(|| self.type_.as_hover_string());
-
-        let is_function = self.type_.is_function_type();
-        if is_function && kind_formatted.trim().is_empty() {
-            kind_formatted = "(function) ".to_owned();
-        }
-        let trimmed_type_display = type_display.trim_start();
-        let needs_def_wrapper = trimmed_type_display.starts_with('(');
-        let (heading, body) = if is_function {
-            let function_name = self.name.clone().unwrap_or_else(|| "function".to_owned());
-            let heading = format!("{}{}\n", kind_formatted, function_name);
-            let body = if needs_def_wrapper {
-                format!("def {}{}: ...", function_name, trimmed_type_display)
-            } else {
-                type_display
-            };
-            (heading, body)
+        let snippet = format_hover_code_snippet(&self.type_, self.name.as_deref(), type_display);
+        let kind_formatted = self.kind.map_or_else(
+            || {
+                snippet
+                    .default_kind_label
+                    .map(str::to_owned)
+                    .unwrap_or_default()
+            },
+            |kind| format!("{} ", kind.display_for_hover()),
+        );
+        let name_formatted = self
+            .name
+            .as_ref()
+            .map_or("".to_owned(), |s| format!("{s}: "));
+        let heading = if let Some(callable_heading) = snippet.heading.as_ref() {
+            format!("{}{}\n", kind_formatted, callable_heading)
         } else {
-            (
-                format!("{}{}", kind_formatted, name_formatted),
-                type_display,
-            )
+            format!("{}{}", kind_formatted, name_formatted)
         };
 
         Hover {
