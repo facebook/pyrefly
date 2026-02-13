1f8a36e2a089279f459be1865d6691d83d2a7147
Asuka Minato <i@asukaminato.eu.org>
2026-02-14 02:45:57 +0900

    impl

--- pyrefly/lib/lsp/non_wasm/server.rs
+++ pyrefly/lib/lsp/non_wasm/server.rs
@@ -268,6 +268,7 @@ use crate::lsp::non_wasm::protocol::write_lsp_message;
 use crate::lsp::non_wasm::queue::HeavyTaskQueue;
 use crate::lsp::non_wasm::queue::LspEvent;
 use crate::lsp::non_wasm::queue::LspQueue;
+use crate::lsp::non_wasm::safe_delete_file::safe_delete_file_code_action;
 use crate::lsp::non_wasm::stdlib::is_python_stdlib_file;
 use crate::lsp::non_wasm::stdlib::should_show_error_for_display_mode;
 use crate::lsp::non_wasm::stdlib::should_show_stdlib_error;
@@ -1117,6 +1118,7 @@ pub fn capabilities(
                 CodeActionKind::QUICKFIX,
                 CodeActionKind::REFACTOR_EXTRACT,
                 CodeActionKind::REFACTOR_REWRITE,
+                CodeActionKind::new("refactor.delete"),
                 CodeActionKind::new("refactor.move"),
                 CodeActionKind::REFACTOR_INLINE,
                 CodeActionKind::SOURCE_FIX_ALL,
@@ -3854,7 +3856,7 @@ impl Server {
 
     fn code_action(
         &self,
-        transaction: &Transaction<'_>,
+        transaction: &mut Transaction<'_>,
         params: CodeActionParams,
         telemetry: &dyn Telemetry,
         activity_key: Option<&ActivityKey>,
@@ -4116,6 +4118,14 @@ impl Server {
             {
                 actions.push(action);
             }
+        }
+        if let Some(action) = safe_delete_file_code_action(
+            &self.initialize_params.capabilities,
+            &self.state,
+            transaction,
+            uri,
+        ) {
+            actions.push(action);
         }
         (!actions.is_empty()).then_some(actions)
     }
