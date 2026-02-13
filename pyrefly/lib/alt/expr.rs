ba517866ea73b488b9d9348278a07e39e3258382
Asuka Minato <i@asukaminato.eu.org>
2026-02-12 18:59:36 -0800

    fix Subscripting on classes should attempt to invoke dunder methods #259 (#2282)
    Summary:
    Fixes https://github.com/facebook/pyrefly/issues/259
    
    Handled class subscripting via the metaclass `__getitem__` before falling back to type specialization, while keeping the existing `__class_getitem__` precedence.
    
    Pull Request resolved: https://github.com/facebook/pyrefly/pull/2282
    
    Test Plan: Added a focused regression test to cover the metaclass case.
    
    Reviewed By: rchen152
    
    Differential Revision: D93171458
    
    Pulled By: stroxler
    
    fbshipit-source-id: 9984f39e0cec50eac64722ab3858d49731ed6825

--- pyrefly/lib/alt/expr.rs
+++ pyrefly/lib/alt/expr.rs
@@ -1930,10 +1930,11 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
                 }
                 Type::ClassDef(cls) => {
                     let metadata = self.get_metadata_for_class(&cls);
-                    let class_getitem_result = if self.get_class_tparams(&cls).is_empty()
+                    let class_ty = Type::ClassDef(cls.dupe());
+                    let allow_dunder_lookup = self.get_class_tparams(&cls).is_empty()
                         && !metadata.has_base_any()
-                        && !metadata.is_new_type()
-                    {
+                        && !metadata.is_new_type();
+                    let class_getitem_result = if allow_dunder_lookup {
                         let class_ty = self.heap.mk_class_def(cls.dupe());
                         // TODO(stroxler): Add a new API, similar to `type_of_attr_get` but returning a
                         // LookupResult or an Optional type, that we could use here to avoid the double lookup.
@@ -1955,7 +1956,21 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
                     } else {
                         None
                     };
-                    if let Some(result) = class_getitem_result {
+                    let metaclass_getitem_result =
+                        if class_getitem_result.is_none() && allow_dunder_lookup {
+                            self.call_magic_dunder_method(
+                                &class_ty,
+                                &dunder::GETITEM,
+                                range,
+                                &[CallArg::expr(slice)],
+                                &[],
+                                errors,
+                                Some(&|| ErrorContext::Index(self.for_display(class_ty.clone()))),
+                            )
+                        } else {
+                            None
+                        };
+                    if let Some(result) = class_getitem_result.or(metaclass_getitem_result) {
                         result
                     } else {
                         self.heap.mk_type_form(self.specialize(
