4270fb17e7ab8f8d70b71cf22c5f69f65fc5d60f
Maxime Arthaud <arthaud@meta.com>
2026-02-17 11:45:57 -0800

    Add missing global variable edges in call graph when using global statement
    Summary:
    We noticed false negatives in Pysa due to missing call graph edges to global variables when the global is accessed after a `global` statement.
    For instance:
    ```
    g = 1
    
    def foo():
      global g
      g = source() # Missing call graph edge
    ```
    
    This diff fixes it, by improving the captured variable algorithm to consider `global g` as pulling a `CapturedKind::Global` as a capture.
    
    Reviewed By: stroxler
    
    Differential Revision: D93483672
    
    fbshipit-source-id: eecc06d73b9494c67fc50ff3218eb1c97b0b4dce

--- pyrefly/lib/report/pysa/call_graph.rs
+++ pyrefly/lib/report/pysa/call_graph.rs
@@ -68,6 +68,7 @@ use crate::report::pysa::ast_visitor::AstScopedVisitor;
 use crate::report::pysa::ast_visitor::ScopeExportedFunctionFlags;
 use crate::report::pysa::ast_visitor::Scopes;
 use crate::report::pysa::ast_visitor::visit_module_ast;
+use crate::report::pysa::captured_variable::CaptureKind;
 use crate::report::pysa::captured_variable::CapturedVariableRef;
 use crate::report::pysa::captured_variable::WholeProgramCapturedVariables;
 use crate::report::pysa::class::ClassRef;
@@ -2430,42 +2431,39 @@ impl<'a> CallGraphVisitor<'a> {
                 )
             })
             .map(|(function, context)| function.as_function_ref(&context))
-        {
             // Skip this path for constructor methods (__init__ and __new__) because they need
             // special handling via resolve_constructor_callees to properly populate init_targets
             // and new_targets. Constructor calls should fall through to the type-based
             // resolution below.
-            let is_constructor_method = function_ref.function_name == dunder::INIT
-                || function_ref.function_name == dunder::NEW;
+            && function_ref.function_name != dunder::INIT
+                && function_ref.function_name != dunder::NEW
+        {
+            let callee_type = self.module_context.answers.get_type_trace(name.range());
+            let callee_expr = Some(AnyNodeRef::from(name));
+            let callee_expr_suffix = Some(name.id.as_str());
 
-            if !is_constructor_method {
-                let callee_type = self.module_context.answers.get_type_trace(name.range());
-                let callee_expr = Some(AnyNodeRef::from(name));
-                let callee_expr_suffix = Some(name.id.as_str());
+            let callees =
+                CallCallees::new(Vec1::new(self.call_target_from_static_or_virtual_call(
+                    function_ref,
+                    callee_expr,
+                    callee_type.as_ref(),
+                    /* precise_receiver_type */ None,
+                    return_type,
+                    callee_expr_suffix,
+                    /* override_implicit_receiver*/ None,
+                    /* override_is_direct_call */ None,
+                    /* unknown_callee_as_direct_call */ true,
+                )));
 
-                let callees =
-                    CallCallees::new(Vec1::new(self.call_target_from_static_or_virtual_call(
-                        function_ref,
-                        callee_expr,
-                        callee_type.as_ref(),
-                        /* precise_receiver_type */ None,
-                        return_type,
-                        callee_expr_suffix,
-                        /* override_implicit_receiver*/ None,
-                        /* override_is_direct_call */ None,
-                        /* unknown_callee_as_direct_call */ true,
-                    )));
-
-                return IdentifierCallees {
-                    if_called: callees,
-                    global_targets: vec![],
-                    captured_variables: vec![],
-                };
-            }
+            return IdentifierCallees {
+                if_called: callees,
+                global_targets: vec![],
+                captured_variables: vec![],
+            };
         }
 
-        // Check if this is a global variable.
-        let global_targets = if let Some(global) =
+        // Check if this is a global variable or captured variable
+        let (global_variable, captured_variable) = if let Some(global) =
             go_to_definition.as_ref().and_then(|definition| {
                 let module_id = self
                     .module_context
@@ -2483,29 +2481,45 @@ impl<'a> CallGraphVisitor<'a> {
                         name: global_var.name.clone(),
                     })
             }) {
-            vec![global]
-        } else {
-            vec![]
-        };
-
-        // Check if this is a captured variable.
-        let captured_variables = if let Some(current_function) = self.current_function.as_ref()
+            (Some(global), None)
+        } else if let Some(current_function) = self.current_function.as_ref()
             && let Some(current_module_captured_variables) = self
                 .captured_variables
                 .get_for_module(self.module_context.module_id)
-            && let Some(captured) = current_module_captured_variables
+            && let Some(captured_variable) = current_module_captured_variables
                 .get(current_function)
                 .and_then(|captured_variables| captured_variables.get(name.id()))
-                .cloned()
-                .map(|outer_function| CapturedVariableRef {
-                    outer_function,
-                    name: name.id().clone(),
-                }) {
-            vec![captured]
+        {
+            match captured_variable {
+                CaptureKind::Local(outer_function) => (
+                    None,
+                    Some(CapturedVariableRef {
+                        outer_function: outer_function.clone(),
+                        name: name.id().clone(),
+                    }),
+                ),
+                CaptureKind::Global
+                    if let Some(global_variables) = self
+                        .global_variables
+                        .get_for_module(self.module_context.module_id)
+                        && global_variables.contains(name.id()) =>
+                {
+                    (
+                        Some(GlobalVariableRef {
+                            module_id: self.module_context.module_id,
+                            module_name: self.module_context.module_info.name(),
+                            name: name.id().clone(),
+                        }),
+                        None,
+                    )
+                }
+                _ => (None, None),
+            }
         } else {
-            vec![]
+            (None, None)
         };
 
+        // Resolve callees using types, if the name is called.
         let callees = self.resolve_callees_from_expression_type(
             /* expression */ Some(AnyNodeRef::from(name)),
             /* expression_type */
@@ -2518,8 +2532,8 @@ impl<'a> CallGraphVisitor<'a> {
         );
         IdentifierCallees {
             if_called: callees,
-            global_targets,
-            captured_variables,
+            global_targets: global_variable.map_or(vec![], |g| vec![g]),
+            captured_variables: captured_variable.map_or(vec![], |c| vec![c]),
         }
     }
 
@@ -3651,7 +3665,7 @@ impl<'a> CallGraphVisitor<'a> {
             }
             _ => None,
         };
-        if let Some(graphql_decorator) = self.matching_graphql_decorators.last().unwrap()
+        if let Some(Some(graphql_decorator)) = self.matching_graphql_decorators.last()
             && let Some(return_expression_type) =
                 return_stmt.value.as_ref().and_then(|return_expression| {
                     self.module_context
