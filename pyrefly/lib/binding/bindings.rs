05139ae3ca74c107dbbdd47c5c192d6295c2d6f0
Asuka Minato <i@asukaminato.eu.org>
2026-02-18 04:56:52 +0900

    .
    .

--- pyrefly/lib/binding/bindings.rs
+++ pyrefly/lib/binding/bindings.rs
@@ -59,6 +59,7 @@ use crate::binding::binding::BindingExport;
 use crate::binding::binding::BindingLegacyTypeParam;
 use crate::binding::binding::BranchInfo;
 use crate::binding::binding::FirstUse;
+use crate::binding::binding::FunctionDefData;
 use crate::binding::binding::FunctionParameter;
 use crate::binding::binding::ImportBinding;
 use crate::binding::binding::Key;
@@ -557,7 +558,7 @@ impl Bindings {
 
     pub(crate) fn pytest_fixture_param_hint(
         &self,
-        def: &StmtFunctionDef,
+        def: &FunctionDefData,
         class_key: Option<&Idx<KeyClass>>,
         param: &Identifier,
     ) -> Option<ShortIdentifier> {
@@ -585,7 +586,7 @@ impl Bindings {
 
     fn is_pytest_test_or_fixture(
         &self,
-        def: &StmtFunctionDef,
+        def: &FunctionDefData,
         class_key: Option<&Idx<KeyClass>>,
         info: &PytestBindingInfo,
     ) -> bool {
