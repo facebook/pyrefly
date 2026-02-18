38c303caee98b5deab2b6765598067e9617477f3
Asuka Minato <i@asukaminato.eu.org>
2026-02-18 23:29:37 +0900

    init

--- lsp/esbuild.js
+++ lsp/esbuild.js
@@ -11,9 +11,13 @@ const production = process.argv.includes('--production');
 const watch = process.argv.includes('--watch');
 
 async function main() {
-  entryPoints = production ? ['src/extension.ts'] : ['src/test/**/*.test.ts', 'src/extension.ts'];
-  const ctx = await esbuild.context({
-    entryPoints: entryPoints,
+  const nodeEntryPoints = production
+    ? ['src/extension.ts']
+    : ['src/test/**/*.test.ts', 'src/extension.ts'];
+  const webEntryPoints = ['src/extension-web.ts'];
+
+  const nodeCtx = await esbuild.context({
+    entryPoints: nodeEntryPoints,
     bundle: true,
     format: 'cjs',
     minify: production,
@@ -28,11 +32,29 @@ async function main() {
       esbuildProblemMatcherPlugin
     ]
   });
+
+  const webCtx = await esbuild.context({
+    entryPoints: webEntryPoints,
+    bundle: true,
+    format: 'cjs',
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: 'browser',
+    outdir: 'dist',
+    external: ['vscode'],
+    logLevel: 'warning',
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin
+    ]
+  });
+
   if (watch) {
-    await ctx.watch();
+    await Promise.all([nodeCtx.watch(), webCtx.watch()]);
   } else {
-    await ctx.rebuild();
-    await ctx.dispose();
+    await Promise.all([nodeCtx.rebuild(), webCtx.rebuild()]);
+    await Promise.all([nodeCtx.dispose(), webCtx.dispose()]);
   }
 }
 
