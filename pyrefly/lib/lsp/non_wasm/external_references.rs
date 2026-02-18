9d56dd9b5e2e80de5aa2d2e4700556e535a90a04
John Van Schultz <jvansch@meta.com>
2026-02-18 08:48:39 -0800

    Add lightweight NoExternalReferences
    Summary: This struct will be used for open source when Glean is not available for O(repo) operations. This struct essentially will always just return an empty vector.
    
    Reviewed By: connernilsen
    
    Differential Revision: D93134506
    
    fbshipit-source-id: 1ad76c413d34266ddc777e4194deb26810e501dd

--- pyrefly/lib/lsp/non_wasm/external_references.rs
+++ pyrefly/lib/lsp/non_wasm/external_references.rs
@@ -11,11 +11,26 @@ use lsp_types::Range;
 use lsp_types::Url;
 
 #[expect(dead_code)]
-pub trait ExternalReferencesProvider {
+pub trait ExternalReferences {
     fn find_references(
         &self,
         qualified_name: &str,
         source_uri: &Url,
         timeout: Duration,
     ) -> Vec<(Url, Vec<Range>)>;
+}
+
+#[expect(dead_code)]
+pub struct NoExternalReferences;
+
+/// This struct will be used when we have no source of external references.
+impl ExternalReferences for NoExternalReferences {
+    fn find_references(
+        &self,
+        _qualified_name: &str,
+        _source_uri: &Url,
+        _timeout: Duration,
+    ) -> Vec<(Url, Vec<Range>)> {
+        Vec::new()
+    }
 }
