af64886752c30824bb4c298279437c7ff5041bf5
Asuka Minato <i@asukaminato.eu.org>
2026-02-21 03:47:43 +0900

    fix test

--- pyrefly/lib/lsp/wasm/completion.rs
+++ pyrefly/lib/lsp/wasm/completion.rs
@@ -684,7 +684,7 @@ impl Transaction<'_> {
                         &ast,
                         self.config_finder(),
                         handle.dupe(),
-                        handle_to_import_from,
+                        handle_to_import_from.dupe(),
                         &name,
                         import_format,
                     );
