9ba01b268a38092915c08638d1cfa17638628f0e
Zeina Migeed <migeedz@meta.com>
2026-03-02 15:28:16 -0800

    Trigger mypy_primer CI (temporary, will revert)

--- pyrefly/lib/commands/init.rs
+++ pyrefly/lib/commands/init.rs
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// TODO: Remove this comment — added to trigger mypy_primer CI for workflow validation
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
