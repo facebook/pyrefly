7a47a381211aa1385974e7a9dd495cc3b0837366
Asuka Minato <i@asukaminato.eu.org>
2026-03-11 09:50:11 +0900

    fix

--- pyrefly/lib/test/decorators.rs
+++ pyrefly/lib/test/decorators.rs
@@ -741,6 +741,26 @@ assert_type(r, int)
     "#,
 );
 
+testcase!(
+    test_class_decorator_rebinds_class_value,
+    r#"
+class ActorHandle[T]:
+    def remote(self) -> T: ...
+    def options(self, num_cpus: int) -> "ActorHandle[T]": ...
+
+def remote[T](cls: type[T]) -> ActorHandle[T]:
+    return ActorHandle()
+
+@remote
+class Worker:
+    def compute(self) -> int:
+        return 42
+
+Worker.remote()
+Worker.options(num_cpus=2)
+"#,
+);
+
 fn env_numba() -> TestEnv {
     let mut env = TestEnv::one_with_path(
         "numba",
