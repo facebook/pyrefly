6a21eb53acc3989ec465ddb9c0091474aa8fd7f8
Matt Van Horn <455140+mvanhorn@users.noreply.github.com>
2026-03-25 09:18:07 -0700

    test: add LSP diagnostic test for deprecated tag
    Verifies that diagnostics for deprecated functions include
    DiagnosticTag::DEPRECATED (tag value 2), which enables strikethrough
    rendering in editors that support LSP diagnostic tags.

---
+++ pyrefly/lib/test/lsp/lsp_interaction/test_files/deprecated_function.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from warnings import deprecated
+
+@deprecated("use new_function instead")
+def old_function() -> None: ...
+
+old_function()
