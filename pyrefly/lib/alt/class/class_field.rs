6325fdf5fdace480a80f95dc42c42df0aaa72649
Asuka Minato <i@asukaminato.eu.org>
2026-04-26 13:34:07 +0900

    fix

--- pyrefly/lib/alt/class/class_field.rs
+++ pyrefly/lib/alt/class/class_field.rs
@@ -2522,7 +2522,8 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
     }
 
     fn attrs_default_for_field(&self, cls: &Class, field_name: &Name) -> Option<Type> {
-        for name in cls.class_body_fields() {
+        let class_fields = self.get_class_fields(cls)?;
+        for name in class_fields.class_body_fields() {
             if name == field_name {
                 continue;
             }
