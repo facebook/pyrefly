22fe26ac3a540568b870152fd0a737ae280bd051
Asuka Minato <i@asukaminato.eu.org>
2026-05-31 15:19:19 +0900

    .

--- pyrefly/lib/test/unittest_mock_patch.rs
+++ pyrefly/lib/test/unittest_mock_patch.rs
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use crate::test::util::TestEnv;
 use crate::testcase;
 
 testcase!(
@@ -15,6 +16,47 @@ from unittest.mock import patch
 def bar() -> None: ...
 
 with patch("main.foo"):  # E: No attribute `foo` in module `main`
+    pass
+"#,
+);
+
+testcase!(
+    test_unittest_mock_patch_target_builtin_checked,
+    r#"
+from unittest.mock import patch
+
+with patch("main.open"):
+    pass
+"#,
+);
+
+testcase!(
+    test_unittest_mock_patch_target_create_true_skipped,
+    r#"
+from unittest.mock import patch
+
+with patch("main.foo", create=True):
+    pass
+"#,
+);
+
+testcase!(
+    test_unittest_mock_patch_target_private_skipped,
+    r#"
+from unittest.mock import patch
+
+with patch("main._MAXLINE"):
+    pass
+"#,
+);
+
+testcase!(
+    test_unittest_mock_patch_target_other_module_skipped,
+    TestEnv::one("other", ""),
+    r#"
+from unittest.mock import patch
+
+with patch("other.foo"):
     pass
 "#,
 );
