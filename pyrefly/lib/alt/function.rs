9dbe1bbb6fe9f15b0671fd9d0a1bd04daa7b4a8a
Asuka Minato <i@asukaminato.eu.org>
2026-07-02 13:37:33 +0900

    fix callable problem

--- pyrefly/lib/alt/function.rs
+++ pyrefly/lib/alt/function.rs
@@ -2130,6 +2130,15 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
         errors: &ErrorCollector,
     ) -> Vec1<(TextRange, OverloadType)> {
         ts.mapped(|(range, t, metadata)| {
+            let display_t = t.clone();
+            let mut t = t;
+            let t = loop {
+                match t {
+                    Type::Intersect(intersect) => t = intersect.1,
+                    Type::KwCall(call) => t = call.return_ty,
+                    t => break t,
+                }
+            };
             (
                 range,
                 match t {
@@ -2181,7 +2190,7 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
                     format!(
                         "`{}` has type `{}` after decorator application, which is not callable",
                         func,
-                        self.for_display(t)
+                        self.for_display(display_t)
                     ),
                 );
                         OverloadType::Function(Function {
