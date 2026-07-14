ab9f80f58719444c6e69be7859f02a63534d3972
Toby Harradine <tobyh@canva.com>
2026-07-15 09:01:48 +1000

    fix(stubgen): extract unpack assignments

---
+++ pyrefly/lib/test/stubgen/unpack_assignments/expected.pyi
@@ -0,0 +1,21 @@
+# @generated
+from typing import ClassVar, Literal
+
+import os
+from enum import Enum
+
+A: Literal[1]
+B: Literal['module']
+PUBLIC: Literal['public']
+FIRST: str
+REST: list[str]
+
+
+class C:
+    A: ClassVar[Literal[3]]
+    B: ClassVar[Literal['class']]
+
+
+class Color(Enum):
+    RED = ...
+    BLUE = ...
---
+++ pyrefly/lib/test/stubgen/unpack_assignments/input.py
@@ -0,0 +1,20 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+from enum import Enum
+
+
+A, B = 1, "module"
+_PRIVATE, PUBLIC = 2, "public"
+FIRST, *REST = os.environ["MY_VAR"].split(" ")
+
+
+class C:
+    A, B = 3, "class"
+
+
+class Color(Enum):
+    RED, BLUE = 1, 2
