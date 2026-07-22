3a1b5bca77995c4fb271e244a92a37b8fb24b4d7
Asuka Minato <i@asukaminato.eu.org>
2026-07-22 17:20:43 +0900

    .

--- pyrefly/lib/alt/solve.rs
+++ pyrefly/lib/alt/solve.rs
@@ -6605,9 +6605,7 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
                     context,
                     preserve_aliases,
                 )?;
-                if let Type::Union(f) = &mut aliased_type
-                    && f.display_name.is_none()
-                {
+                if let Type::Union(f) = &mut aliased_type {
                     f.display_name = Some((self.module().name(), (*ta.name).clone()));
                 }
                 if preserve_aliases {
