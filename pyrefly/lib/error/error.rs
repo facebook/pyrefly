7a1bcb4aa8b791bdd55c6f47780b4d0521e52fde
Asuka Minato <i@asukaminato.eu.org>
2026-07-25 01:17:10 +0900

    fix

--- pyrefly/lib/error/error.rs
+++ pyrefly/lib/error/error.rs
@@ -47,6 +47,7 @@ pub struct SecondaryAnnotation {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum ErrorQuickFix {
     ReplaceWithEnumMember { replacement: String },
+    ReplaceDeprecatedContextManagerReturn { from: String, to: String },
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
