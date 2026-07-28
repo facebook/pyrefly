a3b4c67144e2c1a0e359bba419ed8f796d33a1a4
Asuka Minato <i@asukaminato.eu.org>
2026-07-28 11:45:48 +0900

    comment

---
+++ pyrefly/lib/lsp/non_wasm/server/code_lens/runnable.rs
@@ -0,0 +1,254 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! Detection and rendering of Run and Test CodeLens commands.
+
+use lsp_types::CodeLens;
+use lsp_types::Command;
+use lsp_types::Range;
+use lsp_types::Url;
+use pyrefly_build::handle::Handle;
+use pyrefly_python::ast::Ast;
+use pyrefly_python::module_name::ModuleName;
+use pyrefly_python::module_name::ModuleNameWithKind;
+use pyrefly_python::module_path::ModulePath;
+use pyrefly_util::telemetry::EmptyResponseReason;
+use ruff_python_ast::Expr;
+use ruff_python_ast::ExprAttribute;
+use ruff_python_ast::Stmt;
+use ruff_python_ast::StmtClassDef;
+use ruff_text_size::TextRange;
+use serde_json::Value;
+
+use super::super::Server;
+use crate::state::state::Transaction;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum RunnableKind {
+    Run,
+    Test,
+}
+
+#[derive(Clone, Debug)]
+struct RunnableEntry {
+    range: TextRange,
+    kind: RunnableKind,
+    test_name: Option<String>,
+    class_name: Option<String>,
+    is_unittest: bool,
+}
+
+/// Collect and render all enabled runnable lenses for one Python file.
+pub(super) fn code_lenses(
+    server: &Server,
+    transaction: &Transaction<'_>,
+    handle: &Handle,
+    uri: &Url,
+) -> Result<Vec<CodeLens>, EmptyResponseReason> {
+    let path = server
+        .path_for_uri(uri)
+        .ok_or(EmptyResponseReason::NoFilePath)?;
+    let runnable_code_lens = server
+        .workspaces
+        .get_with(path.clone(), |(_, workspace)| workspace.runnable_code_lens);
+    let maybe_cell_idx = server.maybe_get_code_cell_index(uri);
+    let info = transaction
+        .get_module_info(handle)
+        .ok_or(EmptyResponseReason::ModuleInfoNotFound)?;
+    let entries = runnable_entries(transaction, handle, uri, runnable_code_lens)
+        .ok_or(EmptyResponseReason::ModuleInfoNotFound)?;
+    let config = server.state.config_finder().python_file(
+        ModuleNameWithKind::guaranteed(ModuleName::unknown()),
+        &ModulePath::filesystem(path.clone()),
+    );
+    let cwd = config
+        .source
+        .root_from_file()
+        .map(std::path::Path::to_path_buf)
+        .or_else(|| {
+            server
+                .workspaces
+                .get_with(path, |(workspace_root, _)| workspace_root.cloned())
+        })
+        .map(|path| path.to_string_lossy().into_owned());
+
+    let mut lenses = Vec::new();
+    for entry in entries {
+        if info.to_cell_for_lsp(entry.range.start()) != maybe_cell_idx {
+            continue;
+        }
+        let range = info.to_lsp_range(entry.range);
+        lenses.push(entry_to_lsp_code_lens(uri, range, entry, cwd.as_deref()));
+    }
+    Ok(lenses)
+}
+
+/// Render a semantic runnable entry as an executable LSP command.
+fn entry_to_lsp_code_lens(
+    uri: &Url,
+    range: Range,
+    entry: RunnableEntry,
+    cwd: Option<&str>,
+) -> CodeLens {
+    let (title, command, arguments) = match entry.kind {
+        RunnableKind::Run => {
+            let mut args = serde_json::Map::new();
+            args.insert("uri".to_owned(), serde_json::json!(uri.to_string()));
+            if let Some(cwd) = cwd {
+                args.insert("cwd".to_owned(), serde_json::json!(cwd));
+            }
+            ("Run", "pyrefly.runMain", Some(vec![Value::Object(args)]))
+        }
+        RunnableKind::Test => {
+            let mut args = serde_json::Map::new();
+            args.insert("uri".to_owned(), serde_json::json!(uri.to_string()));
+            if let Some(cwd) = cwd {
+                args.insert("cwd".to_owned(), serde_json::json!(cwd));
+            }
+            args.insert(
+                "position".to_owned(),
+                serde_json::json!({
+                    "line": range.start.line,
+                    "character": range.start.character,
+                }),
+            );
+            if let Some(test_name) = entry.test_name {
+                args.insert("testName".to_owned(), serde_json::json!(test_name));
+            }
+            if let Some(class_name) = entry.class_name {
+                args.insert("className".to_owned(), serde_json::json!(class_name));
+            }
+            args.insert(
+                "isUnittest".to_owned(),
+                serde_json::json!(entry.is_unittest),
+            );
+            ("Test", "pyrefly.runTest", Some(vec![Value::Object(args)]))
+        }
+    };
+
+    CodeLens {
+        range,
+        command: Some(Command {
+            title: title.to_owned(),
+            command: command.to_owned(),
+            arguments,
+            tooltip: None,
+        }),
+        data: None,
+    }
+}
+
+/// Extract runnable entries from the file AST without constructing LSP values.
+fn runnable_entries(
+    transaction: &Transaction<'_>,
+    handle: &Handle,
+    uri: &Url,
+    runnable_code_lens: bool,
+) -> Option<Vec<RunnableEntry>> {
+    if !runnable_code_lens || uri.path().ends_with(".pyi") || uri.path().ends_with(".ipynb") {
+        return Some(Vec::new());
+    }
+    let ast = transaction.get_ast(handle)?;
+    let mut entries = Vec::new();
+    collect_module_entries(&ast.body, &mut entries);
+    Some(entries)
+}
+
+fn collect_module_entries(stmts: &[Stmt], entries: &mut Vec<RunnableEntry>) {
+    for stmt in stmts {
+        match stmt {
+            Stmt::FunctionDef(func) => {
+                maybe_push_test(entries, func.name.as_str(), func.name.range, None, false);
+            }
+            Stmt::ClassDef(class_def) => {
+                let is_unittest = is_unittest_class(class_def);
+                if is_test_class(class_def, is_unittest) {
+                    entries.push(RunnableEntry {
+                        range: class_def.name.range,
+                        kind: RunnableKind::Test,
+                        test_name: None,
+                        class_name: Some(class_def.name.as_str().to_owned()),
+                        is_unittest,
+                    });
+                }
+                collect_class_entries(
+                    &class_def.body,
+                    entries,
+                    class_def.name.as_str(),
+                    is_unittest,
+                );
+            }
+            Stmt::If(stmt_if) if Ast::is_main_guard(&stmt_if.test) => {
+                entries.push(RunnableEntry {
+                    range: stmt_if.range,
+                    kind: RunnableKind::Run,
+                    test_name: None,
+                    class_name: None,
+                    is_unittest: false,
+                });
+            }
+            _ => {}
+        }
+    }
+}
+
+fn collect_class_entries(
+    stmts: &[Stmt],
+    entries: &mut Vec<RunnableEntry>,
+    class_name: &str,
+    is_unittest: bool,
+) {
+    for stmt in stmts {
+        if let Stmt::FunctionDef(func) = stmt {
+            maybe_push_test(
+                entries,
+                func.name.as_str(),
+                func.name.range,
+                Some(class_name.to_owned()),
+                is_unittest,
+            );
+        }
+    }
+}
+
+fn maybe_push_test(
+    entries: &mut Vec<RunnableEntry>,
+    name: &str,
+    range: TextRange,
+    class_name: Option<String>,
+    is_unittest: bool,
+) {
+    if is_test_name(name) {
+        entries.push(RunnableEntry {
+            range,
+            kind: RunnableKind::Test,
+            test_name: Some(name.to_owned()),
+            class_name,
+            is_unittest,
+        });
+    }
+}
+
+fn is_test_name(name: &str) -> bool {
+    name.starts_with("test_")
+}
+
+fn is_test_class(class_def: &StmtClassDef, is_unittest: bool) -> bool {
+    class_def.name.as_str().starts_with("Test") || is_unittest
+}
+
+fn is_unittest_class(class_def: &StmtClassDef) -> bool {
+    class_def.bases().iter().any(is_unittest_base)
+}
+
+fn is_unittest_base(base: &Expr) -> bool {
+    match base {
+        Expr::Name(name) => name.id.as_str().ends_with("TestCase"),
+        Expr::Attribute(ExprAttribute { attr, .. }) => attr.id.as_str().ends_with("TestCase"),
+        _ => false,
+    }
+}
--- pyrefly/lib/test/lsp/lsp_interaction/code_lens.rs
+++ pyrefly/lib/test/lsp/lsp_interaction/code_lens.rs
@@ -5,9 +5,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use std::cell::RefCell;
+use std::path::Path;
+
 use lsp_types::CodeLens;
+use lsp_types::CodeLensOptions;
+use lsp_types::Location;
 use lsp_types::Url;
 use lsp_types::request::CodeLensRequest;
+use lsp_types::request::CodeLensResolve;
 use pyrefly_lsp_test::IndexingMode;
 use pyrefly_lsp_test::LspArgs;
 use pyrefly_lsp_test::object_model::InitializeSettings;
@@ -24,6 +30,76 @@ fn runnable_code_lens_config() -> serde_json::Value {
             "runnableCodeLens": true
         }
     }])
+}
+
+fn indexed_interaction(root_path: &Path) -> LspInteraction {
+    let scope_uri = Url::from_file_path(root_path).unwrap();
+    let mut interaction = LspInteraction::new_with_args(LspInteractionArgs {
+        args: LspArgs {
+            indexing_mode: IndexingMode::LazyBlocking,
+            ..LspInteractionArgs::default().args
+        },
+        ..Default::default()
+    });
+    interaction.set_root(root_path.to_path_buf());
+    interaction
+        .initialize(InitializeSettings {
+            workspace_folders: Some(vec![("test".to_owned(), scope_uri)]),
+            ..Default::default()
+        })
+        .unwrap();
+    interaction
+}
+
+fn request_code_lenses(interaction: &LspInteraction, uri: Url) -> Vec<CodeLens> {
+    let lenses = RefCell::new(Vec::new());
+    interaction
+        .client
+        .send_request::<CodeLensRequest>(json!({
+            "textDocument": {
+                "uri": uri
+            }
+        }))
+        .expect_response_with(|response| {
+            lenses.replace(response.unwrap_or_default());
+            true
+        })
+        .unwrap();
+    lenses.into_inner()
+}
+
+fn resolve_code_lens(interaction: &LspInteraction, lens: CodeLens) -> CodeLens {
+    let resolved = RefCell::new(None);
+    interaction
+        .client
+        .send_request::<CodeLensResolve>(serde_json::to_value(lens).unwrap())
+        .expect_response_with(|lens| {
+            resolved.replace(Some(lens));
+            true
+        })
+        .unwrap();
+    resolved.into_inner().unwrap()
+}
+
+#[test]
+fn test_code_lens_advertises_resolve_provider() {
+    let interaction = LspInteraction::new();
+    interaction
+        .client
+        .send_initialize(
+            interaction
+                .client
+                .get_initialize_params(&InitializeSettings::default()),
+        )
+        .expect_response_with(|response| {
+            response.capabilities.code_lens_provider
+                == Some(CodeLensOptions {
+                    resolve_provider: Some(true),
+                })
+        })
+        .unwrap();
+    interaction.client.send_initialized();
+    interaction.shutdown().unwrap();
 }
 
 #[test]
@@ -214,69 +290,112 @@ fn test_code_lens_disabled_by_default() {
 fn test_code_lens_shows_reference_counts() {
     let root = get_test_files_root();
     let root_path = root.path().join("code_lens_references");
-    let scope_uri = Url::from_file_path(&root_path).unwrap();
     let symbols_uri = Url::from_file_path(root_path.join("symbols.py")).unwrap();
-    let mut interaction = LspInteraction::new_with_args(LspInteractionArgs {
-        args: LspArgs {
-            indexing_mode: IndexingMode::LazyBlocking,
-            ..LspInteractionArgs::default().args
-        },
-        ..Default::default()
-    });
-    interaction.set_root(root_path);
-    interaction
-        .initialize(InitializeSettings {
-            workspace_folders: Some(vec![("test".to_owned(), scope_uri)]),
-            ..Default::default()
-        })
-        .unwrap();
+    let interaction = indexed_interaction(&root_path);
 
     interaction.client.did_open("symbols.py");
     interaction.client.did_open("usage.py");
 
-    interaction
-        .client
-        .send_request::<CodeLensRequest>(json!({
-            "textDocument": {
-                "uri": symbols_uri
-            }
-        }))
-        .expect_response_with(|response| {
-            let Some(lenses) = response else {
-                return false;
-            };
-            has_reference_lens(&lenses, 0, "3 references", 3)
-                && has_reference_lens(&lenses, 1, "2 references", 2)
-                && has_reference_lens(&lenses, 4, "2 references", 2)
-                && lenses.len() == 3
-        })
+    let lenses = request_code_lenses(&interaction, symbols_uri);
+    assert_eq!(lenses.len(), 3);
+    assert!(
+        lenses
+            .iter()
+            .all(|lens| lens.command.is_none() && lens.data.is_some())
+    );
+    for (line, title, count) in [
+        (0, "3 references", 3),
+        (1, "2 references", 2),
+        (4, "2 references", 2),
+    ] {
+        let lens = lenses
+            .iter()
+            .find(|lens| lens.range.start.line == line)
+            .unwrap()
+            .clone();
+        let lens = resolve_code_lens(&interaction, lens);
+        assert!(has_reference_lens(&lens, line, title, count));
+    }
+
+    interaction.shutdown().unwrap();
+}
+
+#[test]
+fn test_reference_code_lens_request_from_notebook_is_empty() {
+    let root = get_test_files_root();
+    let root_path = root.path().join("code_lens_references");
+    let interaction = indexed_interaction(&root_path);
+    interaction.open_notebook("notebook.ipynb", vec!["class NotebookSymbol:\n    pass"]);
+
+    let lenses = request_code_lenses(
+        &interaction,
+        interaction.cell_uri("notebook.ipynb", "cell1"),
+    );
+    assert!(lenses.is_empty());
+
+    interaction.shutdown().unwrap();
+}
+
+#[test]
+fn test_reference_code_lens_excludes_notebook_references() {
+    let root = get_test_files_root();
+    let root_path = root.path().join("code_lens_references");
+    let symbols_uri = Url::from_file_path(root_path.join("symbols.py")).unwrap();
+    let interaction = indexed_interaction(&root_path);
+    interaction.client.did_open("symbols.py");
+    interaction.client.did_open("usage.py");
+    interaction.open_notebook(
+        "notebook.ipynb",
+        vec!["from symbols import Greeter", "Greeter()"],
+    );
+    let notebook_uris = [
+        interaction.cell_uri("notebook.ipynb", "cell1"),
+        interaction.cell_uri("notebook.ipynb", "cell2"),
+    ];
+
+    let lenses = request_code_lenses(&interaction, symbols_uri);
+    let greeter_lens = lenses
+        .into_iter()
+        .find(|lens| lens.range.start.line == 0)
         .unwrap();
+    let resolved = resolve_code_lens(&interaction, greeter_lens);
+    let locations = reference_locations(&resolved);
+    assert_eq!(locations.len(), 3);
+    assert!(
+        locations
+            .iter()
+            .all(|location| !notebook_uris.contains(&location.uri))
+    );
 
     interaction.shutdown().unwrap();
 }
 
 fn has_reference_lens(
-    lenses: &[CodeLens],
+    lens: &CodeLens,
     line: u32,
     expected_title: &str,
     expected_locations: usize,
 ) -> bool {
-    lenses.iter().any(|lens| {
-        let Some(command) = &lens.command else {
-            return false;
-        };
-        if lens.range.start.line != line
-            || command.title != expected_title
-            || command.command != "editor.action.showReferences"
-        {
-            return false;
-        }
-        let Some(arguments) = &command.arguments else {
-            return false;
-        };
+    let Some(command) = &lens.command else {
+        return false;
+    };
+    lens.range.start.line == line
+        && command.title == expected_title
+        && command.command == "editor.action.showReferences"
+        && reference_locations(lens).len() == expected_locations
+}
+
+fn reference_locations(lens: &CodeLens) -> Vec<Location> {
+    let arguments = lens
+        .command
+        .as_ref()
+        .and_then(|command| command.arguments.as_ref())
+        .expect("Resolved reference lens should have command arguments");
+    serde_json::from_value(
         arguments
             .get(2)
-            .and_then(|value| value.as_array())
-            .is_some_and(|locations| locations.len() == expected_locations)
-    })
+            .expect("Reference locations should be the third argument")
+            .clone(),
+    )
+    .expect("Reference locations should deserialize")
 }
