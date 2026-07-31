ea3101afd087c09cd104b4ad55eb6848b768474b
Rebecca Chen <rechen@meta.com>
2026-07-30 17:31:20 -0700

    Allow `super(cls, cls)`
    Summary:
    Fixes https://github.com/facebook/pyrefly/issues/4262.
    
    `super(cls, cls)` previously produced an error because pyrefly didn't know how to handle `type[Self]` as the first argument to a super call. This diff makes it so that we accept `type[Self]` and fall back to `Any` for the looked-up class, since we don't have a concrete class for lookup.
    
    Reviewed By: yangdanny97
    
    Differential Revision: D114193814
    
    fbshipit-source-id: afe3c0c188c5b2c0dd13370f37fe95d884278d15

--- pyrefly/lib/alt/solve.rs
+++ pyrefly/lib/alt/solve.rs
@@ -3257,13 +3257,30 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
     ) -> Type {
         match style {
             SuperStyle::ExplicitArgs(cls_binding, obj_binding) => {
-                match self.get_idx(*cls_binding).ty() {
-                    Type::Any(style) => style.propagate(),
-                    cls_type @ Type::ClassDef(cls) => {
-                        let heap = self.heap;
-                        let make_super_instance = |obj_cls, super_obj: &dyn Fn() -> SuperObj| {
-                            let lookup_cls = self.get_super_lookup_class(cls, obj_cls);
-                            lookup_cls.map_or_else (
+                let cls_answer = self.get_idx(*cls_binding);
+                let cls_type = cls_answer.ty();
+                let (cls, dynamic_start) = match cls_type {
+                    Type::Any(style) => return style.propagate(),
+                    Type::ClassDef(cls) => (cls, false),
+                    Type::Type(inner) if let Type::SelfType(cls) = &**inner => {
+                        (cls.class_object(), true)
+                    }
+                    t => {
+                        return self.error(
+                            errors,
+                            range,
+                            ErrorKind::InvalidArgument,
+                            format!(
+                                "Expected first argument to `super` to be a class object, got `{}`",
+                                self.for_display(t.clone())
+                            ),
+                        );
+                    }
+                };
+                let heap = self.heap;
+                let make_super_instance = |obj_cls, super_obj: &dyn Fn() -> SuperObj| {
+                    let lookup_cls = self.get_super_lookup_class(cls, obj_cls);
+                    lookup_cls.map_or_else (
                                 || {
                                     let cls_type = self.for_display(cls_type.clone());
                                     self.error(
@@ -3274,13 +3291,16 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
                                             "Illegal `super({cls_type}, {obj_cls})` call: `{obj_cls}` is not an instance or subclass of `{cls_type}`"
                                         ),
                                     )
-                                },
-                                |lookup_cls| {
+                            },
+                            |lookup_cls| if dynamic_start {
+                                // `Self` may be any subclass, so there is no sound fixed MRO lookup class.
+                                heap.mk_any_implicit()
+                            } else {
                                     heap.mk_super_instance(lookup_cls, super_obj())
                                 }
                             )
-                        };
-                        match self.get_idx(*obj_binding).ty() {
+                };
+                match self.get_idx(*obj_binding).ty() {
                             Type::Any(style) => style.propagate(),
                             Type::ClassType(obj_cls) => make_super_instance(obj_cls, &|| SuperObj::Instance(obj_cls.clone())),
                             Type::Type(f) if let Type::ClassType(obj_cls) = &**f => {
@@ -3304,17 +3324,6 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
                                     format!("Expected second argument to `super` to be a class object or instance, got `{}`", self.for_display(t.clone())),
                                 )
                             }
-                        }
-                    }
-                    t => self.error(
-                        errors,
-                        range,
-                        ErrorKind::InvalidArgument,
-                        format!(
-                            "Expected first argument to `super` to be a class object, got `{}`",
-                            self.for_display(t.clone())
-                        ),
-                    ),
                 }
             }
             SuperStyle::ImplicitArgs(self_binding, method) => {
