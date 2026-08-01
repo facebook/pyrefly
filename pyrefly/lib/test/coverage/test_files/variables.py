fbf15aec3a22f3cf9a13db0cafd898c35e5bb84f
generatedunixname949130641157030 <noreply+949130641157030@fb.com>
2026-08-01 14:43:55 -0700

    F401 in variables.py
    Differential Revision: D114483532
    
    fbshipit-source-id: 8fbf20696719758fdd9f1c7d1354463a0b11bd07

--- pyrefly/lib/test/coverage/test_files/variables.py
+++ pyrefly/lib/test/coverage/test_files/variables.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Callable, List as MyList, TypeVar
+from typing import Callable, TypeVar
 
 T = TypeVar("T")
 x = 42
