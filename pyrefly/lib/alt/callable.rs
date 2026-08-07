ab4b290eaa7c1509e39af9ae98bcdeb018222e40
Alexander Beedie <alexander.m.beedie@icloud.com>
2026-08-07 13:13:40 +0400

    Significantly optimise checking of repeatedly-instantiated `TypedDict`s

--- pyrefly/lib/alt/callable.rs
+++ pyrefly/lib/alt/callable.rs
@@ -922,8 +922,8 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
         // We want to work mostly with references, but some things are taken from elsewhere,
         // so have some owners to capture them.
         let param_list_owner = Owner::new();
-        let name_owner = Owner::new();
         let type_owner = Owner::new();
+        let fields_owner = Owner::new();
 
         let error = |errors, range, kind, msg: String| {
             self.error_with_context(
@@ -980,9 +980,9 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
                 }
                 TypeOrExpr::Type(ty, _) => match ty {
                     Type::TypedDict(typed_dict) | Type::PartialTypedDict(typed_dict) => {
-                        for (name, field) in self.typed_dict_fields(typed_dict) {
+                        for (name, field) in self.typed_dict_fields(typed_dict).iter() {
                             if field.required {
-                                keyword_arg_names.insert(name);
+                                keyword_arg_names.insert(name.clone());
                             }
                         }
                     }
@@ -1474,22 +1474,22 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
                     kwparams.insert(name, (ty, NameOrigin::Param, required));
                 }
                 Param::Kwargs(name, ty) if let Some(typed_dict) = ty.unpacked_typed_dict() => {
-                    self.typed_dict_fields(typed_dict).into_iter().for_each(
-                        |(field_name, field)| {
-                            kwparams.insert(
-                                name_owner.push(field_name),
-                                (
-                                    type_owner.push(field.ty),
-                                    NameOrigin::UnpackedKwargs(name.as_ref()),
-                                    if field.required {
-                                        &Required::Required
-                                    } else {
-                                        &Required::Optional(None)
-                                    },
-                                ),
-                            );
-                        },
-                    );
+                    for (field_name, field) in
+                        fields_owner.push(self.typed_dict_fields(typed_dict)).iter()
+                    {
+                        kwparams.insert(
+                            field_name,
+                            (
+                                &field.ty,
+                                NameOrigin::UnpackedKwargs(name.as_ref()),
+                                if field.required {
+                                    &Required::Required
+                                } else {
+                                    &Required::Optional(None)
+                                },
+                            ),
+                        );
+                    }
                     kwargs = match self.typed_dict_extra_items(typed_dict) {
                         ExtraItems::Closed => None,
                         ExtraItems::Extra(extra) => {
@@ -1531,7 +1531,7 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
                     let ty = kw.value.infer(self, arg_errors);
                     self.maybe_error_unknown_argument_type(&ty, kw.range, arg_errors);
                     if let Type::TypedDict(typed_dict) = ty {
-                        let fields = self.typed_dict_fields(&typed_dict);
+                        let fields = fields_owner.push(self.typed_dict_fields(&typed_dict));
                         // A non-closed TypedDict may carry arbitrary unknown keys, which can
                         // match the callee's kwargs or any of its unmatched keyword params. An
                         // anonymous TypedDict comes from a dict display, whose keys are all known.
@@ -1586,8 +1586,7 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
                                 },
                             ));
                         }
-                        for (name, field) in fields {
-                            let name = name_owner.push(name);
+                        for (name, field) in fields.iter() {
                             let mut hint = kwargs.as_ref().and_then(|(_, ty)| *ty);
                             if let Some((ty, _, definitely_seen)) = seen_names.get_mut(name) {
                                 // For Required fields, the conflict is guaranteed, so report
