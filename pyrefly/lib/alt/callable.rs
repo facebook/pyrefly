829f85ae48b3a0297b146ee2d24b12bf98ac6b19
Alexander Beedie <alexander.m.beedie@icloud.com>
2026-08-11 11:51:25 +0400

    Check `TypeVar` restrictions when the expected type is gradual

--- pyrefly/lib/alt/callable.rs
+++ pyrefly/lib/alt/callable.rs
@@ -2334,7 +2334,8 @@ impl<'ctx, 'answer, Ans: LookupAnswer> AnswersSolver<'ctx, 'answer, Ans> {
         }
         if let Some(targs) = ctor_targs {
             let residual_vars = call_context.captured_vars();
-            self.solver().generalize_class_targs(targs, &residual_vars);
+            self.solver()
+                .generalize_class_targs(targs, &residual_vars, false);
         }
         let errors = self
             .solver()
