26f9b7f90d1db72079623c054bea57e62fd4223e
Asuka Minato <i@asukaminato.eu.org>
2026-08-17 02:38:40 +0900

    fix

--- pyrefly/lib/test/django/model_serializer.rs
+++ pyrefly/lib/test/django/model_serializer.rs
@@ -25,5 +25,45 @@ class GenericSerializer(serializers.ModelSerializer[MyModel]):
     class Meta:
         model = MyModel
         fields = ["name"]
+
+class ChildSerializer(MySerializer):
+    class Meta:
+        fields = "__all__"
+"#,
+);
+
+django_testcase!(
+    test_declared_fields,
+    r#"
+from rest_framework import serializers
+
+class FieldNameSerializer(serializers.Serializer):
+    label = serializers.CharField()
+    source = serializers.CharField()
+    context = serializers.CharField()
+    data = serializers.CharField()
+
+class NestedSerializer(serializers.Serializer):
+    child = FieldNameSerializer()
+"#,
+);
+
+django_testcase!(
+    test_declared_field_suppression_is_scoped,
+    r#"
+from rest_framework import serializers
+
+class BadSerializer(serializers.Serializer):
+    label = 42  # E: `Literal[42]` is not assignable to attribute `label`
+
+class NotSerializer(serializers.CharField):
+    label = serializers.CharField()  # E: `CharField` is not assignable to attribute `label`
+
+class ParentSerializer(serializers.Serializer):
+    class Meta: ...
+
+class ChildSerializer(ParentSerializer):
+    class Meta:  # E: overrides parent class `ParentSerializer` in an inconsistent manner
+        ...
 "#,
 );
