adb99ea0c1563bccd28dd3d4f85616a9ba0bbb12
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2026-08-19 16:54:54 -0700

    Remove Pyre mode headers
    Reviewed By: maggiemoss
    
    Differential Revision: D116539800
    
    fbshipit-source-id: 656ea5f5f2d6979c43fa26470b6711b0562e1d63

--- test/sarif/validate_sarif.py
+++ test/sarif/validate_sarif.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# pyre-strict
 
 """
 Validate Pyrefly's SARIF output against the official SARIF 2.1.0 JSON Schema.
