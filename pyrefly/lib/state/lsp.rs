732ac370802dffa2af583485d412920d1be94103
lyydsheep <2230561977@qq.com>
2026-08-18 19:06:28 -0700

    Rename legacy type parameter constructor names (#4392)
    Summary:
    Fixes https://github.com/facebook/pyrefly/issues/4348
    
    LSP rename now includes the string name argument in legacy TypeVar and ParamSpec constructor calls. Other strings and constructor calls are unchanged.
    
    Validation:
    - cargo test -p pyrefly test::lsp::rename
    - python3 test.py --no-test --no-tensor-shapes --no-conformance --no-jsonschema
    
    The local `.scratch` planning files are intentionally not part of this PR.
    
    Pull Request resolved: https://github.com/facebook/pyrefly/pull/4392
    
    Reviewed By: grievejia
    
    Differential Revision: D114459691
    
    Pulled By: stroxler
    
    fbshipit-source-id: bfd59a04b352c5d0510ba66d91abfa648ac5e368

--- pyrefly/lib/state/lsp.rs
+++ pyrefly/lib/state/lsp.rs
@@ -69,6 +69,7 @@ use crate::alt::answers::Index;
 use crate::alt::answers_solver::AnswersSolver;
 use crate::alt::attr::AttrDefinition;
 use crate::alt::attr::AttrInfo;
+use crate::binding::binding::Binding;
 use crate::binding::binding::Key;
 use crate::config::error_kind::ErrorKind;
 use crate::error::suppress::detect_line_ending;
@@ -3952,6 +3953,36 @@ impl<'a> Transaction<'a> {
             definition_name,
         ) {
             references.extend(pytest_references);
+        }
+        if let Some(bindings) = self.get_bindings(handle) {
+            let key = Key::Definition(ShortIdentifier::from_text_range(definition_range));
+            if bindings.is_valid_key(&key) {
+                let binding = bindings.get(bindings.key_to_idx(&key));
+                let call = match binding {
+                    Binding::TypeVar(inner) => {
+                        let (_, _, call, _) = inner.as_ref();
+                        Some(call)
+                    }
+                    Binding::ParamSpec(inner) => {
+                        let (_, _, call) = inner.as_ref();
+                        Some(call)
+                    }
+                    Binding::TypeVarTuple(inner) => {
+                        let (_, _, call) = inner.as_ref();
+                        Some(call)
+                    }
+                    _ => None,
+                };
+                if let Some(call) = call {
+                    let name_expr = call.arguments.find_argument_value("name", 0);
+                    if let Some(Expr::StringLiteral(literal)) = name_expr
+                        && let Some(literal) = literal.as_single_part_string()
+                        && literal.value.as_ref() == definition_name.as_str()
+                    {
+                        references.push(literal.content_range());
+                    }
+                }
+            }
         }
         if include_declaration {
             references.push(definition_range);
