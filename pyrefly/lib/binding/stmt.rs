2730e261067cea4f20c61ef6676c8d645a9d344f
Asuka Minato <i@asukaminato.eu.org>
2026-08-27 14:32:30 -0700

    fix False positive for torch.register_* #3988 (#4320)
    Summary:
    Fixes https://github.com/facebook/pyrefly/issues/3988
    
    Recognizes `self.register_buffer(...)` and `self.register_parameter(...)` in `__init__/__post_init__`, synthesizes correctly typed instance attributes for `torch.nn.Module` subclasses.
    
    Pull Request resolved: https://github.com/facebook/pyrefly/pull/4320
    
    Test Plan: add test
    
    Reviewed By: stroxler
    
    Differential Revision: D113871868
    
    fbshipit-source-id: 5821162661e6c9ea7d58227b3512f1074a6ee617

--- pyrefly/lib/binding/stmt.rs
+++ pyrefly/lib/binding/stmt.rs
@@ -1700,6 +1700,42 @@ impl<'a> BindingsBuilder<'a> {
                 } else {
                     None
                 };
+                // PyTorch nn.Module registers buffers and parameters as instance attributes.
+                // Collect literal-name candidates now; solving later verifies nn.Module ancestry.
+                if let Expr::Call(call) = x.value.as_ref()
+                    && let Expr::Attribute(func) = call.func.as_ref()
+                    && let Some(value_keyword) = match func.attr.id.as_str() {
+                        "register_buffer" => Some("tensor"),
+                        "register_parameter" => Some("param"),
+                        _ => None,
+                    }
+                {
+                    let name = call.arguments.args.first().or_else(|| {
+                        call.arguments.keywords.iter().find_map(|keyword| {
+                            (keyword
+                                .arg
+                                .as_ref()
+                                .is_some_and(|arg| arg.as_str() == "name"))
+                            .then_some(&keyword.value)
+                        })
+                    });
+                    let value = call.arguments.args.get(1).or_else(|| {
+                        call.arguments.keywords.iter().find_map(|keyword| {
+                            (keyword
+                                .arg
+                                .as_ref()
+                                .is_some_and(|arg| arg.as_str() == value_keyword))
+                            .then_some(&keyword.value)
+                        })
+                    });
+                    if let (Some(Expr::StringLiteral(name)), Some(value)) = (name, value) {
+                        self.scopes.record_nn_module_registration(
+                            &func.value,
+                            Name::new(name.value.to_str()),
+                            value.clone(),
+                        );
+                    }
+                }
                 let special_export = if let Expr::Call(ExprCall { func, .. }) = &*x.value {
                     self.as_special_export(func)
                 } else {
