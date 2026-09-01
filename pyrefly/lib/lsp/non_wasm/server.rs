af261b04a1edccc01c323d1ee10faa735a71e817
vincent <vincent.ho021@gmail.com>
2026-09-01 09:45:13 +0930

    Removed development comments, whilst leaving the informational comments alongside appropriate code, keeping the structure very similar to the original code.

--- pyrefly/lib/lsp/non_wasm/server.rs
+++ pyrefly/lib/lsp/non_wasm/server.rs
@@ -3333,6 +3333,7 @@ impl Server {
         open_handles: Vec<Handle>,
         f: Option<impl FnOnce(&mut Transaction) + Send + Sync + 'static>,
     ) {
+        // Filter to only include handles from workspaces with streaming enabled
         let streaming_handles: SmallSet<Handle> = open_handles
             .iter()
             .filter(|h| {
@@ -3366,22 +3367,31 @@ impl Server {
         let invalidate_start = Instant::now();
         let has_f = f.is_some();
         if let Some(i) = f {
+            // Mark files as dirty
             i(transaction.as_mut());
         } else {
             transaction.as_mut().invalidate_config();
         }
 
         telemetry_event.set_invalidate_duration(invalidate_start.elapsed());
+
+        // Run transaction prioritizing currently-open files, sending diagnostics as soon as they are available via the subscriber
         server.validate_in_memory_for_transaction(transaction.as_mut(), telemetry_event, None);
 
         if has_f {
+            // Wait in a loop while do_not_commit_recheck flag is set (testing only)
             while server.do_not_commit_recheck.load(Ordering::SeqCst) {
                 std::thread::sleep(std::time::Duration::from_millis(100));
             }
         }
+
+        // Commit will be blocked until there are no ongoing reads.
+        // If we have some long running read jobs that can be cancelled, we should cancel them
+        // to unblock committing transactions.
         for (_, cancellation_handle) in server.cancellation_handles.lock().drain() {
             cancellation_handle.cancel();
         }
+
         // we have to run, not just commit to process updates
         server.state.run_with_committing_transaction(
             transaction,
@@ -3408,61 +3418,7 @@ impl Server {
                     telemetry_event.set_invalidate_find_reason(reason);
                 }
 
-                // f(&transaction.as_mut());
-
                 Self::invalidate_queue(server, telemetry_event, open_handles, Some(f));
-
-                // // Filter to only include handles from workspaces with streaming enabled
-                // let streaming_handles: SmallSet<Handle> = open_handles
-                //     .iter()
-                //     .filter(|h| {
-                //         server
-                //             .workspaces
-                //             .should_stream_diagnostics(h.path().as_path())
-                //     })
-                //     .cloned()
-                //     .collect();
-                // // Store the snapshot so non-committable transactions know not to publish
-                // // diagnostics for these files (they'll be streamed by this transaction)
-                // let has_streaming = !streaming_handles.is_empty();
-                // if has_streaming {
-                //     *server.currently_streaming_diagnostics_for_handles.write() =
-                //         Some(streaming_handles.clone());
-                // }
-                // let publish_callback =
-                //     move |transaction: &Transaction<'_>, handle: &Handle, changed: bool| {
-                //         if changed && streaming_handles.contains(handle) {
-                //             server.publish_for_handles(
-                //                 transaction,
-                //                 std::slice::from_ref(handle),
-                //                 DiagnosticSource::Streaming,
-                //             )
-                //         }
-                //     };
-                // let subscriber = server.make_recheck_subscriber(publish_callback);
-                // let mut transaction = server
-                //     .state
-                //     .new_committable_transaction(Require::Exports, Some(subscriber));
-                // let invalidate_start = Instant::now();
-                // Mark files as dirty
-                // telemetry_event.set_invalidate_duration(invalidate_start.elapsed());
-
-                // Run transaction prioritizing currently-open files, sending diagnostics as soon as they are available via the subscriber
-
-                // Wait in a loop while do_not_commit_recheck flag is set (testing only)
-
-                // Commit will be blocked until there are no ongoing reads.
-                // If we have some long running read jobs that can be cancelled, we should cancel them
-                // to unblock committing transactions.
-
-                // we have to run, not just commit to process updates
-                // server.state.run_with_committing_transaction(
-                //     transaction,
-                //     &[],
-                //     Require::Everything,
-                //     Some(telemetry_event),
-                //     None,
-                // );
 
                 // After we finished a recheck asynchronously, we immediately send `RecheckFinished` to
                 // the main event loop of the server. As a result, the server can do a revalidation of
@@ -5993,56 +5949,6 @@ impl Server {
                     open_handles,
                     None::<fn(&mut Transaction)>,
                 );
-                // let streaming_handles: SmallSet<Handle> = open_handles
-                //     .iter()
-                //     .filter(|h| {
-                //         server
-                //             .workspaces
-                //             .should_stream_diagnostics(h.path().as_path())
-                //     })
-                //     .cloned()
-                //     .collect();
-                // let has_streaming = !streaming_handles.is_empty();
-                // if has_streaming {
-                //     *server.currently_streaming_diagnostics_for_handles.write() =
-                //         Some(streaming_handles.clone());
-                // }
-                // let publish_callback =
-                //     move |transaction: &Transaction<'_>, handle: &Handle, changed: bool| {
-                //         if changed && streaming_handles.contains(handle) {
-                //             server.publish_for_handles(
-                //                 transaction,
-                //                 std::slice::from_ref(handle),
-                //                 DiagnosticSource::Streaming,
-                //             )
-                //         }
-                //     };
-                // let subscriber = server.make_recheck_subscriber(publish_callback);
-                // let mut transaction = server
-                //     .state
-                //     .new_committable_transaction(Require::Exports, Some(subscriber));
-                // let invalidate_start = Instant::now();
-                // transaction.as_mut().invalidate_config();
-                // telemetry_event.set_invalidate_duration(invalidate_start.elapsed());
-                // server.validate_in_memory_for_transaction(
-                //     transaction.as_mut(),
-                //     telemetry_event,
-                //     None,
-                // );
-                // Commit will be blocked until there are no ongoing reads.
-                // If we have some long running read jobs that can be cancelled, we should cancel them
-                // to unblock committing transactions.
-                // for (_, cancellation_handle) in server.cancellation_handles.lock().drain() {
-                //     cancellation_handle.cancel();
-                // }
-                // we have to run, not just commit to process updates
-                // server.state.run_with_committing_transaction(
-                //     transaction,
-                //     &[],
-                //     Require::Everything,
-                //     Some(telemetry_event),
-                //     None,
-                // );
 
                 // After we finished a recheck asynchronously, we immediately send `RecheckFinished` to
                 // the main event loop of the server. As a result, the server can do a revalidation of
