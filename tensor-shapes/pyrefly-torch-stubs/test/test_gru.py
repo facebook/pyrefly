3c9d0e92f18668a8ea222137683050536827221a
Steven Troxler <stroxler@meta.com>
2026-09-20 09:57:33 -0700

    Migrate Torch GRU tests
    Summary:
    Move GRU shape coverage into a dual static and runtime suite. Besides preserving the existing batch-first cases, cover sequence-first, unbatched, symbolic, and invalid-feature inputs, documenting three discrepancies exposed by the runtime cross-check.
    
    ___
    
    Differential Revision: D120867722
    
    fbshipit-source-id: 7d431dc92042c516307d61544d5d1f1931f0459c

---
+++ tensor-shapes/pyrefly-torch-stubs/test/test_gru.py
@@ -0,0 +1,67 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+from typing import assert_type, TYPE_CHECKING
+
+import torch
+import torch.nn as nn
+from shape_extensions import assert_raises, assert_shape, Int, IntVar
+from torch import Tensor
+
+
+def test_gru_batch_first_shapes() -> None:
+    gru = nn.GRU(input_size=256, hidden_size=128, batch_first=True)
+    output, state = gru(torch.ones((4, 10, 256)))
+    assert_shape(output.shape, (4, 10, 128))
+    assert_shape(state.shape, (1, 4, 128))
+
+    bidirectional = nn.GRU(64, 32, bidirectional=True, batch_first=True)
+    output, state = bidirectional(torch.ones((8, 5, 64)))
+    assert_shape(output.shape, (8, 5, 64))
+    assert_shape(state.shape, (2, 8, 32))
+
+    multilayer = nn.GRU(128, 64, num_layers=3, batch_first=True)
+    output, state = multilayer(torch.ones((2, 20, 128)))
+    assert_shape(output.shape, (2, 20, 64))
+    assert_shape(state.shape, (3, 2, 64))
+
+
+def test_gru_sequence_first_shapes() -> None:
+    gru = nn.GRU(6, 4)
+    output, state = gru(torch.ones((5, 2, 6)))
+    assert_shape(output.shape, (5, 2, 4))
+    # TODO: BUG: Use the second input dimension as the batch size.
+    assert_shape(state.shape, (1, 5, 4), runtime=(1, 2, 4))
+
+
+def test_gru_unbatched_shapes() -> None:
+    gru = nn.GRU(6, 4)
+    output, state = gru(torch.ones((5, 6)))
+    # TODO: BUG: Preserve the rank of unbatched GRU inputs and states.
+    assert_shape(output.shape, (5, 6, 4), runtime=(5, 4))
+    assert_shape(state.shape, (1, 5, 4), runtime=(1, 4))
+
+
+def test_gru_rejects_invalid_input_features() -> None:
+    gru = nn.GRU(6, 4, batch_first=True)
+    output, _ = gru(torch.ones((2, 5, 6)))
+    assert_shape(output.shape, (2, 5, 4))
+
+    with assert_raises(RuntimeError):
+        # TODO: BUG: Reject inputs whose last dimension is not `input_size`.
+        gru(torch.ones((2, 5, 7)))
+
+
+if TYPE_CHECKING:
+
+    def check_symbolic_gru[B: IntVar, T: IntVar, N: IntVar, H: IntVar](
+        batch: Int[B], steps: Int[T], inputs: Int[N], hidden: Int[H]
+    ) -> None:
+        gru = nn.GRU(inputs, hidden, batch_first=True)
+        output, state = gru(torch.ones((batch, steps, inputs)))
+        assert_type(output, Tensor[[B, T, H]])
+        assert_type(state, Tensor[[1, B, H]])
